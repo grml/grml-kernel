db561964635c6f2ad7edba96c305a83d2dcb254f
Michael Prokop <mika@grml.org>
2011-03-17 14:09:51 +0000

    Add files for 2.6.38-grml (32bit)

--- scripts/document-buildenv.sh
+++ scripts/document-buildenv.sh
@@ -2,21 +2,24 @@
 # Purpose: capture information regarding the build environment
 # Usage:   cd grml-kernel/2.6.38 ; ../scripts/document-buildenv.sh
 
-echo "Build infrastructure documentation - $(date)" > README.buildenv
+buildenv="README.buildenv.$(dpkg --print-architecture)"
+modules="README.modules.$(dpkg --print-architecture)"
 
-dpkg --list cpp\* gcc\* g++\* libstdc\* libgcc\* binutils\* libc6\* >> README.buildenv
+echo "Build infrastructure documentation - $(date)" > "${buildenv}"
 
-echo "" >> README.buildenv
-echo "# gcc version:" >> README.buildenv
-gcc --version >> README.buildenv
+dpkg --list cpp\* gcc\* g++\* libstdc\* libgcc\* binutils\* libc6\* >> "${buildenv}"
 
-echo "# apt-cache policy:" >> README.buildenv
-apt-cache policy >> README.buildenv
+echo "" >> "${buildenv}"
+echo "# gcc version:" >> "${buildenv}"
+gcc --version >> "${buildenv}"
 
-echo "" >> README.buildenv
-echo "# kernel version:" >> README.buildenv
-echo "$(uname -a)" >> README.buildenv
+echo "# apt-cache policy:" >> "${buildenv}"
+apt-cache policy >> "${buildenv}"
 
-dpkg --list $(grep -v '^#' ../scripts/source_packages) > README.modules.$(dpkg --print-architecture)
+echo "" >> "${buildenv}"
+echo "# kernel version:" >> "${buildenv}"
+echo "$(uname -a)" >> "${buildenv}"
+
+dpkg --list $(grep -v '^#' ../scripts/source_packages) > $modules
 
 ## END OF FILE #################################################################
